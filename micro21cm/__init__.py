ba0dce0ecd79003f2f5b57d4b3706c2dccf2b0b5
Jordan Mirocha <mirochaj@gmail.com>
2021-05-01 12:35:40 -0400

    building FitHelper class to handle annoying things like setting priors and initial walker positions and so forth

--- micro21cm/__init__.py
+++ micro21cm/__init__.py
@@ -1,5 +1,5 @@
-from .inference import FitMCMC
 from .models import BubbleModel
+from .inference import FitHelper
 from .util import labels, get_cmd_line_kwargs
 from .analysis import plot_ps, plot_ps_multi, plot_igm_constraints, \
     get_limits_on_params, read_mcmc, plot_triangle
--- micro21cm/inference.py
+++ micro21cm/inference.py
@@ -27,246 +27,376 @@ except ImportError:
     rank = 0
     size = 1
 
-_default_priors = \
+_priors = \
 {
- 'Ts': (0., 100.),
+ 'Ts': (0., 1000.),
  'Q': (0, 1),
  'R_b': (0, 30),
- 'sigma_b': (0.05, 0.5),
+ 'sigma_b': (0.05, 1.0),
 }
 
-_default_guesses = \
+_guesses = \
 {
- 'Ts': (1., 4.),
+ 'Ts': (5., 150.),
  'Q': (0.2, 0.8),
- 'R_b': (1., 5.),
- 'sigma_b': (0.15, 0.3),
+ 'R_b': (0.5, 10.),
+ 'sigma_b': (0.2, 0.8),
 }
 
-class DummyEnsembleSampler(object):
-    def __init__(self):
-        pass
+_guesses_Q_tanh = {'p0': (6, 10), 'p1': (1, 4)}
+_guesses_R_pl = {'p0': (2., 4.), 'p1': (-2., -1.)}
+
+_priors_Q_tanh = {'p0': (5, 15), 'p1': (0, 20)}
+_priors_R_pl = {'p0': (0, 30), 'p1': (-4, 0.)}
+
+fit_kwargs = \
+{
+ 'fit_z': 5, # If None, fits all redshifts!
+ 'Qprior': True,
+ 'Rprior': True,
+ 'Qxdelta': True, # Otherwise, vary *increments* in Q.
+ 'Rxdelta': True, # Otherwise, vary *increments* in Q.
+ 'Qfunc': None,
+ 'Rfunc': None,
+
+ 'kmax': 1,
+ 'kthin': None,
+
+ 'bubbles_ion': 'ion',      # or 'hot' or False
+ 'bubbles_pdf': 'lognormal',
+ 'include_rsd': 2,
+ 'Rfree': True,
+
+ 'restart': True,
+ 'regroup_after': None,
+ 'steps': 10,
+ 'nwalkers': 128,
+ 'nthreads': 1,
+ 'suffix': None,
+
+}
+
+Q_stagger = lambda z: 1. - min(1, max(0, (z - 5.) / 5.))
+R_stagger = lambda z: ((z - 5.) / 5.)**-2
+
+def tanh_generic(z, zref, dz):
+    return 0.5 * (np.tanh((zref - z) / dz) + 1.)
+
+def power_law(z, norm, gamma):
+    return norm * ((1 + z) / 8.)**gamma
 
 
-class FitGrid(object):
-    def __init__(self, data, model, kblobs=None, invert_logL=True):
+class FitHelper(object):
+    def __init__(self, data, **kwargs):
         self.data = data
-        self.model = model
-        self.kblobs = kblobs
-        self.invert_logL = invert_logL
+        self.kwargs = fit_kwargs.copy()
+        self.kwargs.update(kwargs)
 
-        assert 'k' not in self.data.keys(), \
-            "Format of `data` should be dict of dicts (one per redshift)."
+    def get_model_kwargs(self):
+        kw = {}
+        if self.kwargs['bubbles_ion']:
+            kw['bubbles_ion'] = True
+        else:
+            kw['bubbles_ion'] = False
 
-class FitMCMC(object):
-    def __init__(self, data, model, kblobs=None, invert_logL=True):
-        self.data = data
-        self.model = model
-        self.kblobs = kblobs
-        self.invert_logL = invert_logL
+        kw['bubbles_pdf'] = self.kwargs['bubbles_pdf']
+        kw['include_rsd'] = self.kwargs['include_rsd']
+        kw['bubbles_Rfree'] = self.kwargs['Rfree']
 
-        assert 'k' not in self.data.keys(), \
-            "Format of `data` should be dict of dicts (one per redshift)."
-
-        if size > 1:
-            mpi_err = "Not really setup for MPI parallelism."
-            mpi_err += ' Having likelihood embedded in FitMCMC class causes'
-            mpi_err += ' slowdown related to pickling/unpickling.'
-            mpi_err += ' Use stand-alone script for MPI runs.'
-            raise ValueError(mpi_err)
+        return kw
 
     @property
-    def priors(self):
-        if not hasattr(self, '_priors'):
-            self._priors = _default_priors.copy()
-        return self._priors
+    def fit_zindex(self):
+        if not hasattr(self, '_fit_z'):
+            fit_z = self.kwargs['fit_z']
 
-    def get_initial_walker_pos(self, nwalkers):
-        pos = np.zeros((nwalkers, len(self.model.params)))
+            if type(fit_z) in [list, tuple, np.ndarray]:
+                pass
+            else:
+                fit_z = np.array([fit_z])
 
-        for i, par in enumerate(self.model.params):
-            lo, hi = _default_guesses[par]
+            self._fit_z = np.array(fit_z, dtype=int)
+
+        return self._fit_z
+
+    def get_z_from_index(self, i, data):
+        return data['z'][i]
+
+    @property
+    def prefix(self):
+        if not hasattr(self, '_prefix'):
+            kwargs = self.kwargs
+            if kwargs['bubbles_ion']:
+                prefix = 'bion_{}'.format(kwargs['bubbles_pdf'][0:4])
+            else:
+                prefix = 'bhot_{}'.format(kwargs['bubbles_pdf'][0:4])
+            #else:
+            #    prefix = 'beq0'
+
+            s_prior = ''
+
+            if kwargs['Qfunc'] is not None:
+                prefix += '_Q{}'.format(kwargs['Qfunc'])
+            else:
+                if kwargs['Qxdelta']:
+                    prefix += '_vdQ'
+
+                if kwargs['Qprior']:
+                    s_prior += 'Qinc'
+
+            if kwargs['Rfunc'] is not None:
+                prefix += '_R{}'.format(kwargs['Rfunc'])
+            else:
+                if kwargs['Rfree']:
+                    prefix += '_vRb'
+                else:
+                    prefix += '_vnb'
+
+                if kwargs['Rxdelta']:
+                    prefix += '_vdR'
+
+                if kwargs['Rprior']:
+                    s_prior += 'Rinc'
+
+            prefix += '_mock_21cmfast_{}'.format(kwargs['mocknum'])
+
+            if kwargs['fit_z'] is None:
+                prefix += '_zall'
+                if kwargs['Qprior'] or kwargs['Rprior']:
+                    prefix += '_' + s_prior
+            elif type(kwargs['fit_z']) in [list, tuple, np.ndarray]:
+                s = ''
+                for iz in kwargs['fit_z']:
+                    s += str(int(iz))
+
+                prefix += '_z{}'.format(s)
+                if s_prior.strip():
+                    prefix += '_' + s_prior
+            else:
+                prefix += '_z{}'.format(kwargs['fit_z'])
+
+            if kwargs['kmax'] is not None:
+                prefix += '_kmax_{:.1f}'.format(kwargs['kmax'])
+            if kwargs['kthin'] is not None:
+                prefix += '_kthin_{:.0f}'.format(kwargs['kthin'])
+
+            if kwargs['suffix'] is not None:
+                prefix += '_{}'.format(kwargs['suffix'])
+
+            self._prefix = prefix
+
+        return self._prefix
+
+    @property
+    def func_Q(self):
+        if not hasattr(self, '_func_Q'):
+            if self.kwargs['Qfunc'] is None:
+                self._func_Q = None
+            elif self.kwargs['Qfunc'] == 'tanh':
+                self._func_Q = lambda z, zref, dz: tanh_generic(z, zref, dz)
+            else:
+                raise NotImplemented('help')
+
+        return self._func_Q
+
+    @property
+    def func_R(self):
+        if not hasattr(self, '_func_R'):
+            if self.kwargs['Rfunc'] is None:
+                self._func_R = None
+            elif self.kwargs['Rfunc'] == 'pl':
+                self._func_R = lambda z, norm, index: power_law(z, norm, index)
+            else:
+                raise NotImplemented('help')
+
+        return self._func_R
+
+
+
+    @property
+    def num_parametric(self):
+        if not hasattr(self, '_num_parametric'):
+            self._num_parametric = (self.kwargs['Qfunc'] is not None) \
+                + (self.kwargs['Rfunc'] is not None)
+        return self._num_parametric
+
+    def nparams(self, model):
+        N = (len(model.params) - self.num_parametric) * self.fit_zindex.size
+        N += self.num_parametric * 2
+        return N
+
+    def get_initial_walker_pos(self, model):
+        nwalkers = self.kwargs['nwalkers']
+
+        pos = np.zeros((nwalkers, self.nparams(model)))
+
+        params, redshifts = self.get_param_info(model)
+
+        for i, par in enumerate(params):
+
+            # If parameterized, be careful
+            if redshifts[i] is None:
+                if par.startswith('Q'):
+                    post = par[2:]
+                    lo, hi = _guesses_Q_tanh[post]
+                elif par.startswith('R'):
+                    post = par[2:]
+                    lo, hi = _guesses_R_pl[post]
+                else:
+                    raise NotImplemented('help')
+            else:
+                # Can parameterize change in Q, R_b, rather than Q, R_b
+                # themselves.
+                if (par == 'Q') and self.kwargs['Qxdelta']:
+                    lo, hi = 0, 0.2
+                elif par == 'Q':
+                    Q0 = Q_stagger(redshifts[i])
+                    dQ = 1. / float(len(self.fit_zindex) - 1)
+
+                    lo = Q0 - 0.5 * dQ
+                    hi = Q0 + 0.5 * dQ
+                elif (par == 'R_b') and self.kwargs['Rxdelta']:
+                    lo, hi = 0, 2
+                elif par == 'R_b':
+                    R0 = R_stagger(redshifts[i])
+                    dR = 1. / float(len(self.fit_zindex) - 1)
+
+                    lo = R0 - 0.5 * dR
+                    hi = R0 + 0.5 * dR
+                else:
+                    lo, hi = _guesses[par]
+
             pos[:,i] = lo + np.random.rand(nwalkers) * (hi - lo)
 
         return pos
 
-    def _get_ps(self, z, k, **kwargs):
-        ps_21 = self.model.get_ps_21cm(z=z, k=k, **kwargs)
-        D_21 = ps_21 * k**3 / 2. / np.pi**2
-        return D_21
+    def get_param_info(self, model):
+        """
+        Figure out mapping from parameter list to parameter names and redshifts.
+        """
 
-    def likelihood(self, data, model, err):
-        limits = True
-        if limits:
-            # Likelihood for upper limit with unknown systematic
-            if self.invert_logL:
-                _l_ = -1. * (data - model) / np.sqrt(2.) / err
+        par_per_z = len(model.params)
+        if self.func_Q is not None:
+            par_per_z -= 1
+        if self.func_R is not None:
+            par_per_z -= 1
+
+        ct = 0
+        param_z = []
+        param_names = []
+        for i, iz in enumerate(self.fit_zindex):
+            _z_ = self.get_z_from_index(iz, self.data)
+
+            for j, par in enumerate(model.params):
+
+                if (par == 'Q') and (self.func_Q is not None):
+                    continue
+                if (par == 'R_b') and (self.func_R is not None):
+                    continue
+
+                param_z.append(_z_)
+                param_names.append(par)
+
+
+        # If parameterizing Q or R, these will be at the end.
+        if self.func_Q is not None:
+            param_z.extend([None]*2)
+            param_names.extend(['Q_p0', 'Q_p1'])
+        if self.func_R is not None:
+            param_z.extend([None]*2)
+            param_names.extend(['R_p0', 'R_p1'])
+
+        return param_names, param_z
+
+    def get_param_dict(self, model, z, args, ztol=1e-3):
+        """
+        Take complete list of parameters from emcee and make dictionary
+        of parameters for a single redshift.
+
+        .. note :: If Q(z) is parameterized, will do that calculation here
+            automatically.
+
+        """
+
+        allpars, redshifts = self.get_param_info(model)
+
+
+
+
+        pars = {}
+        for i, par in enumerate(model.params):
+
+            # Check for parametric options
+            if (par == 'Q') and (self.func_Q is not None):
+                pars['Q'] = self.func_Q(z, args[-4], args[-3])
+            elif (par == 'R_b') and (self.func_R is not None):
+                pars['R_b'] = self.func_R(z, args[-2], args[-1])
             else:
-                _l_ = (data - model) / np.sqrt(2.) / err
 
-            L = 0.5 * np.sqrt(np.pi) * (1. + erf(_l_))
-        else:
-            raise NotImplemented('Only dealing with upper limits so far.')
-            #lnL = -0.5 * (np.sum((ymod - ydat)**2 \
-            #        / yerr**2 + np.log(2. * np.pi * yerr**2)))
+                pok = np.zeros(len(allpars))
+                zok = np.zeros(len(allpars))
+                for k, element in enumerate(redshifts):
+                    if element is None:
+                        continue
 
-        return L
+                    if abs(redshifts[k] - z) < ztol:
+                        zok[k] = 1
+                    if allpars[k] == par:
+                        pok[k] = 1
 
-    def loglikelihood(self, pars):
-        """
-        Assumes parameter ordering is: R_b, sigma_b, Q, Ts
-        Assumes args ordering is: ion or heat, k modes, invert likelihood
+                ok = np.logical_and(pok, zok)
 
-        """
+                if ok.sum() == 0:
+                    return None
 
-        pars_dict = {}
-        for i, par in enumerate(self.model.params):
-            pars_dict[par] = pars[i]
+                assert ok.sum() == 1
 
-        Nz = len(self.data.keys())
-        zdata = np.sort(list(self.data.keys()))
+                j = int(np.argwhere(ok==1))
 
-        if zdata.size > 1 and rank == 0:
-            print("WARNING: multi-z fits aren't worthwhile at this stage.")
+                pars[par] = args[j]
 
-        if self.kblobs is not None:
-            blobs_buff = -np.inf * np.ones((zdata.size, len(self.kblobs)))
-        else:
-            blobs_buff = {}
+        if pars == {}:
+            return None
 
-        # Hard-coded priors (sorry)
-        for par in self.model.params:
-            if not (self.priors[par][0] <= pars_dict[par] <= self.priors[par][1]):
-                return -np.inf, blobs_buff
+        return pars
 
-        ##
-        # Enforce priors
-        for par in pars:
-            if par < 0:
-                return -np.inf, blobs_buff
+    def get_prior(self, model, args):
 
-        # Loop over contents of `data`
-        L = []
-        blobs = blobs_buff.copy()
-        for i, z in enumerate(zdata):
-            _data = self.data[z]
-            _kdat = _data['k']
+        params, redshifts = self.get_param_info(model)
 
-            t1 = time.time()
-            ymod = self._get_ps(z, _kdat, **pars_dict)
-            t2 = time.time()
-
-            print("proc {} likelihood eval: {:.2f} sec".format(rank, t2 - t1))
-
-
-            ydat = _data['D_sq']
-            yerr = _data['err']
-
-            # Standard likelihood
-            #lnL = -0.5 * (np.sum((ymod - ydat)**2 \
-            #        / yerr**2 + np.log(2. * np.pi * yerr**2)))
-
-            # Likelihood for upper limit with unknown systematic
-            #if invert:
-            #    _l_ = -1. * (ydat - ymod) / np.sqrt(2.) / yerr
-            #else:
-            #    _l_ = (ydat - ymod) / np.sqrt(2.) / yerr
-
-
-            _l_ = self.likelihood(ydat, ymod, yerr)
-
-            if self.kblobs is not None:
-                blobs[i,:] = self._get_ps(z, kblobs, **pars_dict)
-
-            L.extend(0.5 * np.sqrt(np.pi) * (1. + erf(_l_)))
-
-        lnL = np.sum(np.log(L))
-
-        return lnL, blobs
-
-
-    def run_fit(self, steps=100, nwalkers=64, nthreads=1, prefix=None,
-        restart=False):
-        """
-        Run emcee.
-        """
-
-        args = [self.model, self.data, self.kblobs, self.invert_logL,
-            self.priors]
-
-        Nparams = len(self.model.params)
-
-        if (nthreads == 1) and (size > 1):
-            pool = MPIPool(MPI.COMM_WORLD, use_dill=False)
-
-            if not pool.is_master():
-                pool.wait()
-                sys.exit(0)
-        else:
-            pool = None
-
-        ##
-        # Check for previous output
-        pos = None
-        data_pre = None
-        if prefix is not None:
-            fn = prefix + '.pkl'
-            if os.path.exists(fn) and restart:
-                f = open(fn, 'rb')
-                data_pre = pickle.load(f)
-                f.close()
-
-                nwalkers_p, steps_p, pars_p = data_pre['chain'].shape
-
-                warning = 'Number of walkers must match to enable restart!'
-                warning += 'Previous run ({}) used {}'.format(fn, nwalkers_p)
-                assert nwalkers_p == nwalkers, warning
-
-                print("% Restarting from output {}.".format(fn))
-                print("% Will augment {} samples there with {} more.".format(
-                    steps_p, steps
-                ))
-
-                # Set initial walker positions
-                pos = data_pre['chain'][:,-1,:]
-
-                # Or, use sample_ball to re-initialize
-
-        # Initial guesses for four parameters: R_b, sigma_b, Q, Ts
-        if pos is None:
-            pos = self.get_initial_walker_pos(nwalkers)
-
-        sampler = emcee.EnsembleSampler(nwalkers, Nparams, self.loglikelihood,
-            threads=nthreads, pool=pool)
-
-        print("% Starting MCMC at {}".format(time.ctime()))
-
-        t1 = time.time()
-        results = sampler.run_mcmc(pos, steps)
-        t2 = time.time()
-
-        print("% Ran MCMC for {} steps ({:.1f} minutes).".format(steps,
-            (t2 - t1) / 60.))
-
-        if pool is not None:
-            pool.close()
-
-        ##
-        # Optionally save
-        if prefix is not None:
-
-            if data_pre is not None:
-                chain = data_pre['chain']
-                fchain = data_pre['flatchain']
-
-                data = {'chain': np.concatenate((chain, sampler.chain), axis=1),
-                    'flatchain': np.concatenate((fchain, sampler.flatchain))}
+        Q = []
+        R = []
+        for i, par in enumerate(params):
+            if redshifts[i] is None:
+                if par.startswith('Q'):
+                    post = par[2:]
+                    lo, hi = _priors_Q_tanh[post]
+                elif par.startswith('R'):
+                    post = par[2:]
+                    lo, hi = _guesses_R_pl[post]
+                else:
+                    raise NotImplemented('help')
             else:
-                data = {'chain': sampler.chain, 'flatchain': sampler.flatchain}
+                # Can parameterize change in Q, R_b, rather than Q, R_b
+                # themselves.
+                lo, hi = _priors[par]
 
-            with open(fn, 'wb') as f:
-                pickle.dump(data, f)
+            if par == 'Q':
+                Q.append(args[i])
+            elif par == 'R':
+                R.append(args[i])
 
-            print("% Wrote {}.".format(fn))
+            if not (lo <= args[i] <= hi):
+                return -np.inf
 
-        return sampler
+
+        if self.kwargs['Qprior'] and (self.func_Q is None):
+            if not np.all(np.diff(Q) < 0):
+                return -np.inf
+        if self.kwargs['Rprior'] and (self.func_R is None):
+            if not np.all(np.diff(R) < 0):
+                return -np.inf
+
+
+        # If we made it this far, everything is OK
+        return 0.

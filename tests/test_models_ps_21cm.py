d5d8cadef2db67e647f90a6f9bee1b493c56fb66
Jordan Mirocha <mirochaj@gmail.com>
2021-09-13 22:13:10 -0400

    fixed ps_21cm test

--- tests/test_models_ps_21cm.py
+++ tests/test_models_ps_21cm.py
@@ -17,7 +17,7 @@ def test():
     model = micro21cm.BubbleModel()
 
     # Set modes of interest
-    z = 8
+    z = 8.
     k = np.logspace(-1., 0, 5)
 
     # Compute P(k)
@@ -26,9 +26,9 @@ def test():
     ps_pre = np.zeros(k.size)
     for Ts in [0.5 * Tcmb, 0.1 * Tcmb]:
 
-        ps = model.get_ps_21cm(z=z, k=k, Ts=Ts, Q=0.5, R=3., sigma=1)
-        assert np.all(ps > ps_pre)
-        ps_pre = ps 
+        ps = model.get_ps_21cm(z=z, k=k, Ts=Ts, Q=0.2, R=3., sigma=1)
+        assert np.all(ps > ps_pre), "{} {}".format(ps, ps_pre)
+        ps_pre = ps
 
 
 if __name__ == '__main__':

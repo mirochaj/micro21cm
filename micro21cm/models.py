63cb715c6409ec75a143d659618971749e432c99
Jordan Mirocha <mirochaj@gmail.com>
2021-09-22 19:52:31 -0400

    remove method_Pbn option

--- micro21cm/models.py
+++ micro21cm/models.py
@@ -41,7 +41,6 @@ class BubbleModel(object):
         include_P1=True, include_P2=True,
         include_P1_corr=False, include_P2_corr=False, include_overlap_corr=0,
         include_cross_terms=1, include_rsd=2, include_mu_gt=-1.,
-        method_Pbn=1,
         use_volume_match=1, density_pdf='lognormal',
         Rmin=1e-2, Rmax=1e4, NR=1000,
         omega_b=0.0486, little_h=0.67, omega_m=0.3089, ns=0.96,
@@ -151,7 +150,6 @@ class BubbleModel(object):
         self.use_volume_match = use_volume_match
         self.approx_linear = approx_linear
         self.density_pdf = density_pdf
-        self.method_Pbn = method_Pbn
 
         self.params = ['Ts', 'Q']
         if self.bubbles:
